8cf144a1fc65bc2b9221030fdbd07691041a6dcf
BeSl <belyakovva@me.com>
2025-02-15 22:03:21 +0300

    mvp проекта

---
+++ uin_cheker/src/main/kotlin/org/besl/uin_cheker/UinChekerApplication.kt
@@ -0,0 +1,11 @@
+package org.besl.uin_cheker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UinChekerApplication
+
+fun main(args: Array<String>) {
+	runApplication<UinChekerApplication>(*args)
+}
---
+++ uin_cheker/src/test/kotlin/org/besl/uin_cheker/UinChekerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.besl.uin_cheker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UinChekerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
