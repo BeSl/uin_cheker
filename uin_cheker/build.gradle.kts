62f4c141bfee4049f9bda089fbdfce4fe258e63d
BeSl <belyakovva@me.com>
2025-02-27 22:31:42 +0300

    добавил страницу ui

--- uin_cheker/build.gradle.kts
+++ uin_cheker/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.vaadin") version "24.6.5"
 }
 
 group = "org.besl"
@@ -18,6 +19,8 @@ repositories {
 	mavenCentral()
 }
 
+extra["vaadinVersion"] = "24.6.5"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -27,12 +30,19 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("com.vaadin:vaadin-spring-boot-starter")
+
 	implementation(files("C:\\opencv\\build\\java\\opencv-4100.jar"))
 //	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //	runtimeOnly("org.postgresql:postgresql")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 	implementation("org.jsoup:jsoup:1.13.1")
+	implementation("com.vaadin:vaadin-spring-boot-starter")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+	}
 }
 
 kotlin {
